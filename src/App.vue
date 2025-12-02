178352271330365db5b009cba2f77437223664d2
Kristen Wishart <kwishart24@gmail.com>
2025-12-02 18:24:10 -0500

    first commit. 10 min into the initial YouTube video setting up project, installed Tailwind

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import SearchInput from './components/SearchInput.vue';
+</script>
+
+<template>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{new Date().toLocaleString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })}}
+    </div>
+
+    <!-- Search -->
+    <div>
+      <SearchInput/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
