79f17a003953cc85bde66aa8d909284cfe5b7814
Kristen Wishart <kwishart24@gmail.com>
2025-12-03 17:50:24 -0500

    fixed Tailwindcss and imported tailwindcss into main.css to fix the rendering of the searchbar

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
 // postcss.config.js
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
