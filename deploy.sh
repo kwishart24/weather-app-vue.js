231e712916886d05c5b8df9444123f1885b2a4b6
Kristen Wishart <kwishart24@gmail.com>
2025-12-09 03:33:26 -0500

    adding back the vite node_modules I accidentally delted and added base

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:kwishart24/weather-app-vue.js.git main:gh-pages3
+git push -f git@github.com:kwishart24/weather-app-vue.js.git main:gh-pages
 
 cd -
