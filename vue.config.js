0507a01aaad6e34a2379a3a58adedaa71ab612d5
Kristen Wishart <kwishart24@gmail.com>
2025-12-09 02:21:52 -0500

    trying to fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-// import { defineConfig } from '@vue/cli-service'
+import { defineConfig } from '@vue/cli-service'
 
-// export default defineConfig({
-//   transpileDependencies: true,
-//   publicPath: '/weather-app-vue.js/',
-// })
-
-/* eslint-env node */
-module.exports = {
+export default defineConfig({
+  transpileDependencies: true,
   publicPath: '/weather-app-vue.js/',
-}
+})
+
+// /* eslint-env node */
+// module.exports = {
+//   publicPath: '/weather-app-vue.js/',
+// }
