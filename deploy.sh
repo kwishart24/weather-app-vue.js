8feb51bc1def9ac8d36f6c0facc7f84d9e7156be
Kristen Wishart <kwishart24@gmail.com>
2025-12-08 23:22:11 -0500

    added deployment file for deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env sh
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f git@github.com:kwishart24/weather-app-vue.js.git main:gh-pages
+
+cd -
---
+++ vue.conf.js
@@ -0,0 +1,6 @@
+import { defineConfig } from '@vue/cli-service'
+
+export default defineConfig({
+  transpileDependencies: true,
+  publicPath: '/weather-app-vue.js/',
+})
